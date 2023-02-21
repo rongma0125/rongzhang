5ac4eb6b647fbbd01a7bac3d8755324558ff8284
rongma0125 <627846957@qq.com>
2023-02-21 17:01:17 +0800

    Delete certbot-ci directory

--- certbot-ci/certbot_integration_tests/nginx_tests/context.py
+++
@@ -1,5 +0,0 @@
-from certbot_integration_tests.certbot_tests import context as certbot_context
-
-
-class IntegrationTestsContext(certbot_context.IntegrationTestsContext):
-    """General fixture describing a certbot-nginx integration tests context"""
